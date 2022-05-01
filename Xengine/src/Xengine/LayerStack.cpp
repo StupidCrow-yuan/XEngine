96ae8ec25411bc4bfefe29703a32c1a9addc5ff2
StupidCrow <719817649@qq.com>
2022-05-01 21:10:52 +0800

    fix(change): move onattach and ondetach to layerstack

--- Xengine/src/Xengine/LayerStack.cpp
+++ Xengine/src/Xengine/LayerStack.cpp
@@ -22,12 +22,14 @@ namespace XEngine {
     void LayerStack::PushLayer(Layer *layer)
     {
         m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
+        layer->OnAttach();
         m_LayerInsertIndex++;
     }
 
     void LayerStack::PushOverlay(Layer *overlay)
     {
         m_Layers.emplace_back(overlay);
+        overlay->OnAttach();
     }
 
     void LayerStack::PopLayer(Layer *layer)
@@ -37,6 +39,7 @@ namespace XEngine {
         {
             m_Layers.erase(it);
             m_LayerInsertIndex--;
+            layer->OnDetach();
         }
     }
 
@@ -46,6 +49,7 @@ namespace XEngine {
         if (it != m_Layers.end())
         {
             m_Layers.erase(it);
+            overlay->OnDetach();
         }
     }
 }
