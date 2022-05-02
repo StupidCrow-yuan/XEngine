3aa7c169f6edd3841b0211dd9637d083b69a97ef
StupidCrow <719817649@qq.com>
2022-05-02 20:31:33 +0800

    fix(bug): fix macro name

--- Xengine/src/Xengine/Log.h
+++ Xengine/src/Xengine/Log.h
@@ -23,17 +23,17 @@ namespace XEngine {
 }
 
 // Core log macros
-#define XE_CORE_TRACE(...)    ::XEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define XE_CORE_INFO(...)     ::XEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define XE_CORE_WARN(...)     ::XEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define XE_CORE_ERROR(...)    ::XEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define XE_CORE_FATAL(...)    ::XEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define XE_CORE_TRACE(...)          ::XEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define XE_CORE_INFO(...)           ::XEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define XE_CORE_WARN(...)           ::XEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define XE_CORE_ERROR(...)          ::XEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define XE_CORE_CRITICAL(...)       ::XEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
-#define XE_TRACE(...)	      ::XEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define XE_INFO(...)	      ::XEngine::Log::GetClientLogger()->info(__VA_ARGS__)
-#define XE_WARN(...)	      ::XEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define XE_ERROR(...)	      ::XEngine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define XE_FATAL(...)	      ::XEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define XE_TRACE(...)               ::XEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define XE_INFO(...)	            ::XEngine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define XE_WARN(...)	            ::XEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define XE_ERROR(...)	            ::XEngine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define XE_CRITICAL(...)	        ::XEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
 
 #endif //XENGINEMAIN_LOG_H
