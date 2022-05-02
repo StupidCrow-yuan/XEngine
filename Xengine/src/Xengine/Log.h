55c980b83c451e9e18e19a2d0587fd6f20f6f375
StupidCrow <719817649@qq.com>
2022-05-02 10:29:23 +0800

    fix(bug): The last log level in spdlog is level::critical and not level::fatal and the method is critical() not fatal()

--- Xengine/src/Xengine/Log.h
+++ Xengine/src/Xengine/Log.h
@@ -27,13 +27,13 @@ namespace XEngine {
 #define XE_CORE_INFO(...)     ::XEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define XE_CORE_WARN(...)     ::XEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define XE_CORE_ERROR(...)    ::XEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define XE_CORE_FATAL(...)    ::XEngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define XE_CORE_FATAL(...)    ::XEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define XE_TRACE(...)	      ::XEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define XE_INFO(...)	      ::XEngine::Log::GetClientLogger()->info(__VA_ARGS__)
 #define XE_WARN(...)	      ::XEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define XE_ERROR(...)	      ::XEngine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define XE_FATAL(...)	      ::XEngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define XE_FATAL(...)	      ::XEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
 
 #endif //XENGINEMAIN_LOG_H
