6fd27c9b839f3b5093a5ffcc8e8c9e4d722e3ae0
StupidCrow <719817649@qq.com>
2022-05-02 20:44:00 +0800

    fix(bug): layerStack only need to search for the layer to be pop in its corresp

--- Xengine/src/Xengine/LayerStack.cpp
+++ Xengine/src/Xengine/LayerStack.cpp
@@ -34,7 +34,7 @@ namespace XEngine {
 
     void LayerStack::PopLayer(Layer *layer)
     {
-        auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+        auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
         if (it != m_Layers.end())
         {
             layer->OnDetach();
@@ -45,7 +45,7 @@ namespace XEngine {
 
     void LayerStack::PopOverlay(Layer *overlay)
     {
-        auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
+        auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
         if (it != m_Layers.end())
         {
             overlay->OnDetach();
