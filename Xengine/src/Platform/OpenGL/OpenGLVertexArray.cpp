82bdce3269a6bdaa97c4867ad085693272d2871a
StupidCrow <719817649@qq.com>
2022-05-04 16:33:25 +0800

    fix(bug): OpenGLVertexArray not adding multible VertexBuffers correctly

--- Xengine/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Xengine/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -60,8 +60,8 @@ namespace XEngine {
         const auto& layout = vertexBuffer->GetLayout();
         for (const auto& element : layout)
         {
-            glEnableVertexAttribArray(index);
-            glVertexAttribPointer(index, element.GetComponentCount(),
+            glEnableVertexAttribArray(index + m_VertexBufferIndexOffset);
+            glVertexAttribPointer(index + m_VertexBufferIndexOffset, element.GetComponentCount(),
                                   ShaderDataTypeToOpenGLBaseType(element.Type),
                                   element.Normalized ? GL_TRUE : GL_FALSE,
                                   layout.GetStride(),
@@ -69,6 +69,7 @@ namespace XEngine {
             index++;
         }
         m_VertexBuffers.push_back(vertexBuffer);
+        m_VertexBufferIndexOffset += layout.GetElements().size();
     }
 
     void OpenGLVertexArray::SetIndexBuffer(const std::shared_ptr<IndexBuffer> &indexBuffer)
