ae69e7e052878a4d33ee474aae4a05cdeb3a3c24
StupidCrow <719817649@qq.com>
2022-06-04 20:48:13 +0800

    fix(bug): Renderer2D now uses a single shader to render both textured and non-textured quads

--- Xengine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Xengine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -3,11 +3,25 @@
 //
 
 #include "xepch.h"
-#include <glad/glad.h>
 #include "OpenGLTexture.h"
 #include "stb_image.h"
 
 namespace XEngine {
+
+    OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height) : m_Width(width), m_Height(height)
+    {
+        m_InternalFormat = GL_RGBA8;
+        m_DataFormat = GL_RGBA;
+
+        glGenTextures(1, &m_RendererID);
+        glBindTexture(GL_TEXTURE_2D, m_RendererID);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    }
+
     OpenGLTexture2D::OpenGLTexture2D(const std::string &path)
     {
         int width, height, channels;
@@ -28,6 +42,9 @@ namespace XEngine {
             internalFormat = GL_RGB8;
             dataFormat = GL_RGB;
         }
+
+        m_InternalFormat = internalFormat;
+        m_DataFormat = dataFormat;
         XE_CORE_ASSERT(internalFormat &dataFormat, "Format not supported!");
 
         glGenTextures(1, &m_RendererID);
@@ -55,6 +72,13 @@ namespace XEngine {
     OpenGLTexture2D::~OpenGLTexture2D()
     {
         glDeleteTextures(1, &m_RendererID);
+    }
+
+    void OpenGLTexture2D::SetData(void *data, uint32_t size)
+    {
+        uint32_t  bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+        XE_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
+        glTexImage2D(GL_TEXTURE_2D, 0, m_InternalFormat, m_Width, m_Height, 0, m_DataFormat, GL_UNSIGNED_BYTE, data);
     }
 
     void OpenGLTexture2D::Bind(uint32_t slot) const
