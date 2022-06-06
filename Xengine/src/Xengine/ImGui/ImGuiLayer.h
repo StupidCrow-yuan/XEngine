3462f8160124c2def669bc8043147a37a13e0115
StupidCrow <719817649@qq.com>
2022-06-06 20:56:08 +0800

    feat(remove): remove imgui demo window

--- Xengine/src/Xengine/ImGui/ImGuiLayer.h
+++ Xengine/src/Xengine/ImGui/ImGuiLayer.h
@@ -18,7 +18,6 @@ namespace XEngine {
 
         virtual void OnAttach() override;
         virtual void OnDetach() override;
-        virtual void OnImGuiRender() override;
 
         void Begin();
         void End();
