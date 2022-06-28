c540f8ab4c91ec08c8df3e9134b1467e4abd748a
StupidCrow <719817649@qq.com>
2022-06-28 21:27:36 +0800

    feat(log): Log messages are now additionally sent to XEngine.log file

--- Xengine/src/Xengine/Core/Log.cpp
+++ Xengine/src/Xengine/Core/Log.cpp
@@ -5,6 +5,7 @@
 #include "xepch.h"
 #include "Log.h"
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace XEngine {
     Ref<spdlog::logger> Log::s_CoreLogger;
@@ -12,11 +13,21 @@ namespace XEngine {
 
     void Log::Init()
     {
-        spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("XENGINE");
-        s_CoreLogger->set_level(spdlog::level::trace);
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(CPP_SRC_DIR"Sandbox/XEngine.log", true));
 
-        s_CoreClientLogger = spdlog::stdout_color_mt("APP");
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+        s_CoreLogger = std::make_shared<spdlog::logger>("XENGINE", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreLogger);
+        s_CoreLogger->set_level(spdlog::level::trace);
+        s_CoreLogger->flush_on(spdlog::level::trace);
+
+        s_CoreClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreClientLogger);
         s_CoreClientLogger->set_level(spdlog::level::trace);
+        s_CoreClientLogger->flush_on(spdlog::level::trace);
     }
 }
