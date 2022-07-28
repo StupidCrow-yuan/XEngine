e60c24bec848c1f261d899395e33a4c1c92a01a6
StupidCrow <719817649@qq.com>
2022-07-28 21:15:36 +0800

    Renderer2D can now render rotated quads in batches

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -36,14 +36,19 @@ void Sandbox2D::OnUpdate(XEngine::Timestep ts)
     }
 
     {
+        static float rotation = 0.0f;
+        rotation += ts * 50.0f;
+        
         XE_PROFILE_SCOPE("Renderer Draw");
         XEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
-//        XEngine::Renderer2D::DrawRotatedQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+        
+        XEngine::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
         XEngine::Renderer2D::DrawQuad({-1.0, 0.0f}, {0.8f, 0.8f}, { 0.8f, 0.2f, 0.3f, 1.0f });
         XEngine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 
-        XEngine::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckboardTexture, 10.0f);
-        XEngine::Renderer2D::DrawQuad({ -0.50f, -0.50f, 0.0f }, { 1.0f, 1.0f }, m_CheckboardTexture, 20.0f);
+        XEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckboardTexture, 10.0f);
+        XEngine::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckboardTexture, 20.0f);
+        
         XEngine::Renderer2D::EndScene();
     }
 
