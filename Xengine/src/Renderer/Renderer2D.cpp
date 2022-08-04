f74433bcfbb916f2cb36dee83147dc50d2dd469f
StupidCrow <719817649@qq.com>
2022-08-04 09:52:30 +0800

    fix(bug): Updating submodules from origin and fix waring

--- Xengine/src/Renderer/Renderer2D.cpp
+++ Xengine/src/Renderer/Renderer2D.cpp
@@ -128,7 +128,7 @@ namespace XEngine {
     {
         XE_PROFILE_FUNCTION();
 
-        uint32_t dataSize = (uint8_t*)s_Data.QuadVertexBufferPtr - (uint8_t*)s_Data.QuadVertexBufferBase;
+        uint32_t dataSize = (uint32_t)((uint8_t*)s_Data.QuadVertexBufferPtr - (uint8_t*)s_Data.QuadVertexBufferBase);
         s_Data.QuadVertexBuffer->SetData(s_Data.QuadVertexBufferBase, dataSize);
 
         Flush();
