d2928f64038b631e2858b28525d37b3e4055ef56
StupidCrow <719817649@qq.com>
2022-09-04 21:27:08 +0800

    fix(update): 重构 input 类

--- Xengine/src/Platform/Windows/WindowsInput.cpp
+++ Xengine/src/Platform/Windows/WindowsInput.cpp
@@ -2,27 +2,27 @@
 // Created by yuan on 4/18/22.
 //
 
-#include "WindowsInput.h"
+#include "Xengine/Core/Input.h"
 #include "Xengine/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace XEngine {
 
-    bool WindowsInput::IsKeyPressedImpl(KeyCode key)
+    bool Input::IsKeyPressed(KeyCode key)
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetKey(window, static_cast<int32_t>(key));
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
-    bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+    bool Input::IsMouseButtonPressed(MouseCode button)
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
         return state == GLFW_PRESS;
     }
 
-    std::pair<float, float> WindowsInput::GetMousePositionImpl()
+    std::pair<float, float> Input::GetMousePosition()
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         double xpos, ypos;
@@ -30,17 +30,17 @@ namespace XEngine {
         return {(float)xpos, (float)ypos };
     }
 
-    float WindowsInput::GetMouseXImpl()
+    float Input::GetMouseX()
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-        auto[x, y] = GetMousePositionImpl();
+        auto[x, y] = GetMousePosition();
         return x;
     }
 
-    float WindowsInput::GetMouseYImpl()
+    float Input::GetMouseY()
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-        auto[x, y] = GetMousePositionImpl();
+        auto[x, y] = GetMousePosition();
         return y;
     }
 }
