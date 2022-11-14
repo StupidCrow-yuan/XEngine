a89f99aa2f4523fd326cef7d6ea91159e5c4bdcc
StupidCrow <719817649@qq.com>
2022-11-14 22:05:06 +0800

    feat(framebuffer):Refactored framebuffers to allow for MRTs

--- Xengine/src/Platform/OpenGL/OpenGLFramebuffer.h
+++ Xengine/src/Platform/OpenGL/OpenGLFramebuffer.h
@@ -24,14 +24,17 @@ namespace XEngine
 
         virtual void ReadPixel(const std::string& path) override;
 
-        virtual uint32_t GetColorAttachmentRendererID() const override { return m_ColorAttachment; };
+        virtual uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const override { XE_CORE_ASSERT(index < m_ColorAttachments.size()); return m_ColorAttachments.size() > 0 ? m_ColorAttachments[index] : 0; };
 
         virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
     private:
         uint32_t m_RendererID = 0;
-        uint32_t m_ColorAttachment = 0;
+        std::vector<uint32_t> m_ColorAttachments;
         uint32_t m_DepthAttachment = 0;
         FramebufferSpecification m_Specification;
+
+        std::vector<FramebufferTextureSpecification> m_ColorAttachmentSpecifications;
+        FramebufferTextureSpecification m_DepthAttachmentSpecification = FramebufferTextureFormat::None;
     };
 }
 
